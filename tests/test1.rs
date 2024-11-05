0c41659d1966e8c6ad65ba7219cb96ac447f16e0
周睿 <zrufo747@outlook.com>
2024-11-05 10:14:44 +0800

    init

---
+++ tests/test1.rs
@@ -0,0 +1,32 @@
+#![no_std]
+#![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(bare_test::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
+extern crate bare_test;
+
+#[bare_test::entry]
+fn main() {
+    test_main();
+}
+
+use bare_test::{iomap, println};
+#[test_case]
+fn it_works2() {
+    println!("test2... ");
+    assert_eq!(1, 2);
+}
+#[test_case]
+fn it_works1() {
+    println!("test1... ");
+    assert_eq!(1, 1);
+}
+
+#[test_case]
+fn test_uart() {
+    // map uart data register for using.
+    let uart_data_reg = iomap(0x9000000.into(), 0x1000);
+
+    println!("uart test passed!");
+}
